4377f03edd81fe4537296feaf9bcae64e48f4831
sneekers707 <VladislavOzerin@yandex.ru>
2025-07-15 22:59:12 +0300

    Update app.py
    Add app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-# Название может быть: app.py, main.py или server.py
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Привет! Сервер работает."
+    return "Приложение работает!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=10000)
