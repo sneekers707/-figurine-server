7cf842a65e8c8de72ac47029244ecb7830db5635
sneekers707 <VladislavOzerin@yandex.ru>
2025-07-16 23:25:14 +0300

    Update app.py
    Add app.py

--- app.py
+++ app.py
@@ -26,5 +26,6 @@ def submit():
     return render_template('result.html', filename=filename, accessories=accessories)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
