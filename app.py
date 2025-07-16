384ee6ef232bd68a42f2943385c96fcfafdc2243
sneekers707 <VladislavOzerin@yandex.ru>
2025-07-16 22:48:36 +0300

    Update app.py
    Add app.py

--- app.py
+++ app.py
@@ -1,47 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
-from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'static/uploads'
-app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg'}
 
-if not os.path.exists(app.config['UPLOAD_FOLDER']):
-    os.makedirs(app.config['UPLOAD_FOLDER'])
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+UPLOAD_FOLDER = 'static/uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/generate', methods=['POST'])
-def generate():
-    if 'photo' not in request.files:
-        return "Ошибка: файл не найден", 400
+@app.route('/submit', methods=['POST'])
+def submit():
+    photo = request.files['photo']
+    accessories = request.form.getlist('accessories')
 
-    file = request.files['photo']
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(filepath)
+    if photo:
+        photo_path = os.path.join(app.config['UPLOAD_FOLDER'], photo.filename)
+        photo.save(photo_path)
+        filename = photo.filename
     else:
-        return "Неверный формат файла", 400
+        filename = None
 
-    # Получение выбранных аксессуаров
-    accessory1 = request.form.get('accessory1')
-    accessory2 = request.form.get('accessory2')
-    accessory3 = request.form.get('accessory3')
-
-    accessories = [accessory1, accessory2, accessory3]
-
-    return render_template('result.html', image_url=filepath, accessories=accessories)
-
-@app.route('/success')
-def success():
-    return "Приложение работает!"
+    return render_template('result.html', filename=filename, accessories=accessories)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
